8237a8844822fa7b50d88c510d9096752dde53a8
Uriah <codeitlikemiley@gmail.com>
2024-08-03 03:52:21 +0800

    impl user handler

---
+++ src/handlers.rs
@@ -0,0 +1,46 @@
+use uuid::Uuid;
+
+use crate::{
+    commands::CreateUser,
+    db,
+    events::UserCreated,
+    models::{self},
+    repositories::UserRepository,
+};
+
+#[derive(Clone)]
+pub struct UserHandler {
+    pub repo: db::PgPool,
+}
+
+impl UserHandler {
+    pub fn new(repo: db::PgPool) -> Self {
+        Self { repo }
+    }
+
+    pub async fn handle_create_user(&self, cmd: CreateUser) -> Result<(), sqlx::Error> {
+        let user = models::User {
+            id: Uuid::now_v7(),
+            username: cmd.username,
+            email: cmd.email,
+        };
+
+        let event = UserCreated {
+            id: user.id,
+            username: user.username.clone(),
+            email: user.email.clone(),
+        };
+
+        self.repo.save_user(user).await?;
+        self.repo.save_event(event).await?;
+
+        Ok(())
+    }
+
+    pub async fn handle_get_user_by_id(
+        &self,
+        id: Uuid,
+    ) -> Result<Option<models::User>, sqlx::Error> {
+        self.repo.find_user_by_id(id).await
+    }
+}
