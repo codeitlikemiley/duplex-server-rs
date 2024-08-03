c26adc3f4891d5c4dec97f0c7fa8785ae8b99dd7
Uriah <codeitlikemiley@gmail.com>
2024-08-03 16:44:10 +0800

    add Command Marker Trait

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,11 @@
 use serde::Deserialize;
 
+use crate::domain::Command;
+
 #[derive(Deserialize, Debug)]
 pub struct CreateUser {
     pub username: String,
     pub email: String,
 }
+
+impl Command for CreateUser {}
