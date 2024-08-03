c93db987fd9ed6567d4bf9a69d8b01bc277f56d9
Uriah <codeitlikemiley@gmail.com>
2024-08-03 16:44:30 +0800

    use compile time check on sqlx

--- src/db.rs
+++ src/db.rs
@@ -18,28 +18,31 @@ impl PgPool {
 #[async_trait]
 impl UserRepository for PgPool {
     async fn save_user(&self, user: models::User) -> Result<(), sqlx::Error> {
-        sqlx::query("INSERT INTO users (id,username,email) VALUES ($1,$2,$3)")
-            .bind(user.id)
-            .bind(&user.username)
-            .bind(&user.email)
-            .execute(&self.db)
-            .await?;
+        sqlx::query!(
+            "INSERT INTO users (id,username,email) VALUES ($1,$2,$3)",
+            user.id,
+            &user.username,
+            &user.email,
+        )
+        .execute(&self.db)
+        .await?;
         Ok(())
     }
 
     async fn save_event(&self, event: UserCreated) -> Result<(), sqlx::Error> {
         let payload = serde_json::to_value(&event).unwrap();
-        sqlx::query("INSERT INTO events (id,payload) VALUES ($1,$2)")
-            .bind(Uuid::now_v7())
-            .bind(payload)
-            .execute(&self.db)
-            .await?;
+        sqlx::query!(
+            "INSERT INTO events (id,payload) VALUES ($1,$2)",
+            Uuid::now_v7(),
+            payload
+        )
+        .execute(&self.db)
+        .await?;
         Ok(())
     }
 
     async fn find_user_by_id(&self, id: Uuid) -> Result<Option<models::User>, sqlx::Error> {
-        sqlx::query_as::<_, models::User>("SELECT * from users WHERE id = $1")
-            .bind(id)
+        sqlx::query_as!(models::User, "SELECT * from users WHERE id = $1", id)
             .fetch_optional(&self.db)
             .await
     }
