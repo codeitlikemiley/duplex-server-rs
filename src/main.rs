f1b12fb83de8f833101bd0051356efb4070fcea8
Uriah <codeitlikemiley@gmail.com>
2024-08-03 14:17:40 +0800

    use controller on routes

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
 mod commands;
+mod controllers;
 mod db;
 mod events;
 mod handlers;
 mod models;
 mod repositories;
 use axum::{
-    extract::{Json, Path, State},
-    response::IntoResponse,
     routing::{get, post},
     Router,
 };
+use controllers::{create_user, get_user_by_id};
 use handlers::UserHandler;
 use sqlx::postgres::PgPoolOptions;
 use std::time::Duration;
-use tracing::{error, info};
-use uuid::Uuid;
 
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -52,40 +50,4 @@ async fn main() {
         .unwrap();
 
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn create_user(
-    State(handler): State<UserHandler>,
-    Json(payload): Json<commands::CreateUser>,
-) -> impl IntoResponse {
-    match handler.handle_create_user(payload).await {
-        Ok(_) => {
-            info!("User Created");
-            "User created".into_response()
-        }
-        Err(_) => {
-            error!("Failed to Create User");
-            "Failed to create user".into_response()
-        }
-    }
-}
-
-async fn get_user_by_id(
-    State(state): State<handlers::UserHandler>,
-    Path(id): Path<Uuid>,
-) -> impl IntoResponse {
-    match state.handle_get_user_by_id(id).await {
-        Ok(Some(user)) => {
-            info!("User Found:\n {:#?}", user);
-            Json(user).into_response()
-        }
-        Ok(None) => {
-            info!("User Not Found");
-            "User not found".into_response()
-        }
-        Err(_) => {
-            error!("Failed to Fetch User");
-            "Failed to get user".into_response()
-        }
-    }
 }
