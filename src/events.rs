27684d0ec5d9fb3fa69faf860f3fb37ac7b83b6b
Uriah <codeitlikemiley@gmail.com>
2024-08-03 16:44:18 +0800

    add event marker trait

--- src/events.rs
+++ src/events.rs
@@ -1,9 +1,12 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+use crate::domain::Event;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserCreated {
     pub id: Uuid,
     pub username: String,
     pub email: String,
 }
+impl Event for UserCreated {}
