7456d32c9fa63dd8753c2f7d214931010a3af18f
Uriah <codeitlikemiley@gmail.com>
2024-08-04 18:03:30 +0800

    distribute request either for http or grpc apis

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use coqrs::{db, init_logger, listener, router};
+use axum::{extract::Request, http::header::CONTENT_TYPE};
+use coqrs::{db, init_logger, router, services};
+use tower::{make::Shared, steer::Steer};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<(), anyhow::Error> {
@@ -8,11 +10,27 @@ async fn main() -> anyhow::Result<(), anyhow::Error> {
 
     let pool = db::pgpool_connections().await;
 
-    let app = router(pool);
+    let lb = Steer::new(
+        [router(pool.clone()), services(pool)],
+        |req: &Request, _services: &[_]| {
+            req.headers()
+                .get(CONTENT_TYPE)
+                .map(|content_type| content_type.as_bytes())
+                .filter(|content_type| content_type.starts_with(b"application/grpc"))
+                .map(|_| 1)
+                .unwrap_or(0)
+        },
+    );
 
-    let listener = listener().await;
+    let listener = tokio::net::TcpListener::bind("[::]:80").await.unwrap();
 
-    axum::serve(listener, app).await?;
+    tracing::debug!("listening on {:?}", listener.local_addr().unwrap());
+
+    let server = axum::serve(listener, Shared::new(lb)).await;
+
+    if let Err(err) = server {
+        tracing::error!("server error: {:?}", err);
+    }
 
     Ok(())
 }
