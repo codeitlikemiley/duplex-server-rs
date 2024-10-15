8b1bd93040b6a7d3b4a4c5435f6cbebfee9d7732
Uriah <codeitlikemiley@gmail.com>
2024-10-15 19:49:32 +0800

    added up and down migrations for users

--- migrations/20240802165529_create_users_table.up.sql
+++ migrations/20240802165529_create_users_table.up.sql
@@ -1,4 +1,3 @@
--- Add migration script here
 CREATE TABLE users (
     id UUID PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE, 
--- migrations/20240802165536_create_events_table.sql
+++
@@ -1,5 +0,0 @@
--- Add migration script here
-CREATE TABLE events (
-    id UUID PRIMARY KEY,
-    payload JSONB NOT NULL
-)
