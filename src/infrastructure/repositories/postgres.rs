d1f127b73807c84f735ec8bf3c510ab71e7050b0
codeitlikemiley <codeitlikemiley@gmail.com>
2025-09-13 12:42:26 +0800

    ✅ T206 - Update PostgreSQL repository to handle new User fields

--- src/infrastructure/repositories/postgres.rs
+++ src/infrastructure/repositories/postgres.rs
@@ -1,5 +1,67 @@
-rc/infrastructure/repositories/postgres.rs</path>
-<content">    async fn find_user_by_email(&self, email: &str) -> Result<Option<models::User>, sqlx::Error> {
+use async_trait::async_trait;
+use sqlx::{Pool, Postgres};
+use uuid::Uuid;
+
+use crate::{events::UserCreated, models, repositories::{UserRepository, UserProfileRepository}};
+
+#[derive(Clone, Debug)]
+pub struct PostgreSQL {
+    db: Pool<Postgres>,
+}
+
+impl PostgreSQL {
+    pub fn new(db: Pool<Postgres>) -> Self {
+        Self { db }
+    }
+}
+
+#[async_trait]
+impl UserRepository for PostgreSQL {
+    async fn save_user(&self, user: models::User) -> Result<(), sqlx::Error> {
+        let result = sqlx::query!(
+            "INSERT INTO users (id,username,email,password_hash,email_verified,status,created_at,updated_at,last_login_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)",
+            user.id,
+            &user.username,
+            &user.email,
+            &user.password_hash,
+            user.email_verified,
+            user.status as models::UserStatus,
+            user.created_at,
+            user.updated_at,
+            user.last_login_at
+        )
+        .execute(&self.db)
+        .await?;
+        if result.rows_affected() > 1 {
+            let event = UserCreated {
+                id: user.id,
+                username: user.username.clone(),
+                email: user.email.clone(),
+            };
+            self.save_event(event).await?
+        }
+        Ok(())
+    }
+
+    async fn save_event(&self, event: UserCreated) -> Result<(), sqlx::Error> {
+        let payload = serde_json::to_value(&event).unwrap();
+        sqlx::query!(
+            "INSERT INTO events (id,payload) VALUES ($1,$2)",
+            Uuid::now_v7(),
+            payload
+        )
+        .execute(&self.db)
+        .await?;
+        Ok(())
+    }
+
+    async fn find_user_by_id(&self, id: Uuid) -> Result<Option<models::User>, sqlx::Error> {
+        sqlx::query_as!(models::User, "SELECT * from users WHERE id = $1", id)
+            .fetch_optional(&self.db)
+            .await
+    }
+
+    async fn find_user_by_email(&self, email: &str) -> Result<Option<models::User>, sqlx::Error> {
         sqlx::query_as!(models::User, "SELECT * from users WHERE email = $1", email)
             .fetch_optional(&self.db)
             .await
